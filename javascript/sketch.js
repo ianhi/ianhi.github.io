94888bbed3457167b9a2f3a17a390b0f686d836f
ianhi <ianhuntisaak@gmail.com>
2015-12-12 14:56:02 -0500

    perlin walker update

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -1,31 +1,70 @@
-var bug;  // Declare object
-
+var walkers = [];  // Declare object
+var moreButton,lessButton,saveButton,resetButton,colorButton;
+var totalWalkers=0;
+var colorScheme=0;
 function setup() {
-  //createCanvas(700,400);
- var myCanvas = createCanvas(800, 600);
- myCanvas.parent('myContainer');
- //myCanvas.position(800,800)
-  // Create object
+  var myCanvas = createCanvas(800, 600);
+  myCanvas.parent('myContainer');
+  background(50, 89, 100);
+  for (i=0;i<3;i++){
+    walkers[i] = new Walker((totalWalkers)*20000);
+    totalWalkers += 1;
+  }
+  moreButton= createButton(' + ','asdf');
+     moreButton.parent('myContainer2');
+     moreButton.mousePressed(more);
+  lessButton = createButton(' - ');
+     lessButton.parent('myContainer2');
+     lessButton.mousePressed(less)
+  saveButton = createButton('save');
+     saveButton.parent('myContainer2');
+     saveButton.mousePressed(saveWithName)
+  resetButton = createButton('reset');
+     resetButton.parent('myContainer2');
+     resetButton.mousePressed(reset);
+  colorButton = createButton('colors');
+     colorButton.parent('myContainer2');
+     colorButton.mousePressed(toggleColors);
+}
+function more(){
+  curLen = walkers.length;
+  walkers[curLen] = new Walker(totalWalkers*20000);
+  totalWalkers +=1
+}
+function less(){
+  walkers = walkers.slice(0,walkers.length-1);
+}
+function reset(){
+  if (mouseX >= 0 && mouseX <= width){
     background(50, 89, 100);
-  bug = new Jitter();
+    for(i=0;i<walkers<length;i++){
+      walkers[i].x=random()*width;
+      walkers[i].y=random()*height;
+    }
+  }
 }
-
+function toggleColors(){
+  colorScheme = (colorScheme+1)%3;
+}
 function draw() {
-
-  bug.display();
-  bug.move();
-
+  for(i=0;i<walkers.length;i++){
+  walkers[i].display();
+  walkers[i].move();
 }
+  //bug.display();
+  //bug.move();
+}
+
 
 // Jitter class
-function Jitter() {
+function Walker(t_offset) {
   this.x=0;
   this.y=0;
-  this.tx=0;
-  this.ty=10000;
+  this.tx=0+t_offset;
+  this.ty=10000+t_offset;
   this.maxStep=10;
   this.diameter=10;
-
+  this.offset = t_offset;
   this.move = function() {
    var stepx = map(noise(this.tx),0,1,-this.maxStep,this.maxStep);
    var stepy = map(noise(this.ty),0,1,-this.maxStep,this.maxStep);
@@ -44,11 +83,37 @@ function Jitter() {
   };
 
   this.display = function() {
+    if (colorScheme==0){
+      fill(255);
+      stroke(0);
+    }
+    else if (colorScheme==1) {
+      var Color = map(noise(this.tx+50000),0,1,0,255);
+      var alpha = map(noise(this.tx+2500000),0,1,0,255)-this.tx/100000;
+      noStroke();
+      fill(Color,alpha)
+    }
+    else if (colorScheme==2) {
+      var r =  map(noise(this.tx+50000),0,1,0,255);
+      var g =  map(noise(this.tx+100000),0,1,0,255);
+      var b =  map(noise(this.tx+1500000),0,1,0,255);
+      var alpha = map(noise(this.tx+2500000),0,1,0,255)-this.tx/100000;
+      noStroke();
+      fill(r,g,b,alpha)
+    }
     ellipse(this.x, this.y, this.diameter, this.diameter);
   }
 };
-function mousePressed(){
-      background(50, 89, 100);
-      bug.x=random()*width;
-      bug.y=random()*height;
+
+function keyPressed(){
+  if(key==' '){
+    saveWithName();
+  }
+
+}
+function saveWithName(){
+  var today = new Date();
+  var min = today.getMinutes();
+  var sec = today.getSeconds();
+  save('perlinWalker-'+min+':'+sec+'.png');
 }
